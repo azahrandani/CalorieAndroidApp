83ed380a2639782c30473ca2238d107363be5d05
Azahra Putri <azahra.putri@ui.ac.id>
2019-11-09 21:52:54 +0700

    Change API url max

--- app/src/main/java/com/lab/calorie/service/GetDataService.java
+++ app/src/main/java/com/lab/calorie/service/GetDataService.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface GetDataService {
 
-    String URL = "/ndb/nutrients/?format=json&api_key=XTq2zCQifXnxMuDXUSB0scIWcJvKF2UMFjT3fAj8&nutrients=208&subset=1&max=15";
+    String URL = "/ndb/nutrients/?format=json&api_key=XTq2zCQifXnxMuDXUSB0scIWcJvKF2UMFjT3fAj8&nutrients=208&subset=1&max=200";
 
     @GET(URL)
     Call<CompleteFoodJson> getFoodJson();
